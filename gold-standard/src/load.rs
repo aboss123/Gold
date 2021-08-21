9b59ac170bd95b4920be747a8d7066b703d31b2b
noah <nomaxx117@gmail.com>
2021-08-21 15:39:14 -0500

    Fixed linter issues

--- gold-standard/src/load.rs
+++ gold-standard/src/load.rs
@@ -2,11 +2,14 @@ use std::mem::transmute;
 
 use cranelift_jit::JITBuilder;
 
+use crate::io::{print, println};
+use crate::mem::{free, malloc};
+
 const SYMBOLS: [(&str, *const u8); 4] = [
-    ("print", unsafe { transmute(&crate::io::print) }),
-    ("println", unsafe { transmute(&crate::io::println) }),
-    ("malloc", unsafe { transmute(&crate::mem::malloc) }),
-    ("free", unsafe { transmute(&crate::mem::free) }),
+    ("print", unsafe { transmute(print as unsafe extern "C" fn(_) -> _) }),
+    ("println", unsafe { transmute(println as unsafe extern "C" fn(_) -> _) }),
+    ("malloc", unsafe { transmute(malloc as unsafe extern "C" fn(_) -> _) }),
+    ("free", unsafe { transmute(free as unsafe extern "C" fn(_)) }),
 ];
 
 pub fn load_symbols(jit_builder: &mut JITBuilder) {
