c6503ef5ec5c9275f1bd0728c8f1c71558c69dcc
noah <nomaxx117@gmail.com>
2021-08-22 12:47:30 -0500

    Now using cli args

--- gold/src/main.rs
+++ gold/src/main.rs
@@ -5,9 +5,11 @@ use gold_backend::analysis::lower::Analyzer;
 use gold_frontend::frontend;
 use gold_frontend::parse::Parser;
 use gold_backend::codegen::Compilation;
+use std::env::args;
 
 fn main() -> Result<(), String> {
-    let mut parser = Parser::new("tests/another.gold")?;
+    let file = args().skip(1).next().expect("Need a file path please!");
+    let mut parser = Parser::new(&file)?;
     let expr = parser.parse_file()?;
     let mut analyze = Analyzer::new(parser.file_contents, parser.filename.to_owned());
     expr.typecheck(&mut analyze);
